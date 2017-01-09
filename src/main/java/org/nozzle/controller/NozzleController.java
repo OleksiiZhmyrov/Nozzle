6547b736f99fae96b307eb0f73735745d44fae35
Yuriy Tkachenko <yuriy_tkachenko@epam.com>
2017-01-09 07:55:39 +0200

    add web controller

--- src/main/java/org/nozzle/controller/NozzleController.java
+++ src/main/java/org/nozzle/controller/NozzleController.java
@@ -1,12 +1,14 @@
 package org.nozzle.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NozzleController {
 
-    @RequestMapping("/")
+    @RequestMapping("/nozzle")
     public String index() {
         return "Hello, Nozzle!";
     }
